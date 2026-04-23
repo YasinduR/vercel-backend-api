06ff791b6a30d45d13d3e872f9486abc701c0faf
yasindug <ysndrmt@gmail.com>
2026-04-23 12:14:18 +0530

    dealer login

---
+++ api/dealer/login.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    return res.status(200).json(
+{
+    "success": true,
+    "message": "Successfully logged",
+    "data": [
+    ],
+    "errors": null
+}
+  );
+  }
+  return res.status(405).json({ message: "Method not allowed" });
+}
