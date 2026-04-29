6e39b95ef1c336ba957d22b3b498512e40d4d7f1
yasindug <ysndrmt@gmail.com>
2026-04-29 14:36:25 +0530

    OTPSENT RESPONSE CHANGED

--- api/user/change-password.js
+++ api/user/change-password.js
@@ -2,16 +2,17 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
     return res.status(200).json(
 {
-    "success": true,
-    "message": "Temporary password requested successfully",
-    "data": {
-        "phone": "*****2424"
-    },
-    "errors": null
+ "success": true,
+ "message": "Password changed successfully",
+ "data": {},
+ "errors": null
 }
-
     );
   }
   return res.status(405).json({ message: "Method not allowed" });
 }
+
+
+
+
 
